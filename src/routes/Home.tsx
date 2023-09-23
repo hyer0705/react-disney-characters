cd6bd675a6d2018e02dff1535c246cf5ef55f704
hyer0705 <hyer1205@gmail.com>
2023-09-23 23:27:25 +0900

    Use createBrowserRouter to set the router

---
+++ src/routes/Home.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function Home() {
+  return (
+    <Container>
+      <h1>Disney Characters</h1>
+    </Container>
+  );
+}
+
+export default Home;
