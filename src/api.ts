6017791a4ed60252d6064403db55f9137caed781
hyer0705 <hyer1205@gmail.com>
2023-09-23 23:40:01 +0900

    Use the Disney Characters API to show a character list

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 const BASE_URL = `https://disney_api.nomadcoders.workers.dev`;
 
 export function fetchAllCharacters() {
-  return fetch(`${BASE_URL}/characters`).then((resp) => resp.json());
+  return fetch(`${BASE_URL}/characters`)
+    .then((resp) => resp.json())
+    .then((json) => json.slice(0, 100)); // 임시로 100개만 가져오기
 }
 
 export function fetchCharacterById(id: number) {
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,44 @@
 import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { fetchAllCharacters } from "../api";
 
 const Container = styled.div``;
 
+const Header = styled.header``;
+
+const Title = styled.h1``;
+
+const CharacterList = styled.ul``;
+
+interface ICharacter {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
 function Home() {
+  const { isLoading, data: characters } = useQuery<ICharacter[]>({
+    queryKey: ["allCharacters"],
+    queryFn: fetchAllCharacters,
+  });
+
   return (
     <Container>
-      <h1>Disney Characters</h1>
+      <Header>
+        <Title>Disney Characters</Title>
+      </Header>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <CharacterList>
+          {characters?.map((character) => (
+            <li key={character.id}>
+              <img src={character.imageUrl} alt={character.name} />
+              <span>{character.name}</span>
+            </li>
+          ))}
+        </CharacterList>
+      )}
     </Container>
   );
 }
